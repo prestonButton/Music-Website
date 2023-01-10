c8ce78ed627bcee1279cdd16f1bb1b1bb68d5606
prestonButton <prestonbutton99@gmail.com>
2023-01-10 12:21:27 -0500

    edit modal works, just have to get it so not all feilds have to be changed

--- scripts.js
+++ scripts.js
@@ -65,8 +65,6 @@ const loginSubmit = document.getElementById("loginSubmit");
 let adminMode = false;
 
 let numEvents = 0;
-
-const events = {};
 /*************************************************************************** */
 
 //functions
@@ -138,7 +136,7 @@ document.addEventListener("keydown", (e) => {
 //      -user is presented with an error message
 //      -the add event modal is not closed
 //
-//3. when user clicks on edit button, user is presented with the edit event modal
+//3. For all instances of editButton, when user clicks on edit button, user is presented with the edit event modal
 //  a) the modal fields are populated with the current event information
 //  b) if user enters all required fields, the event is updated
 //      -the edit event modal is closed
@@ -156,7 +154,7 @@ document.addEventListener("keydown", (e) => {
 
 /*************************************************************************** */
 
-//1. when user clicks on admin button, 
+//1. when user clicks on admin button,
 //      -user is presented with the login modal
 
 //  a) if user enters correct username and password, admin mode is turned on
@@ -177,59 +175,61 @@ document.addEventListener("keydown", (e) => {
 //      -Admin button text is changed to "Admin"
 
 // when user clicks on admin button
-adminButton.addEventListener('click', () => {
+adminButton.addEventListener("click", () => {
   // if admin mode is off
   if (!adminMode) {
     // reset login form
     loginForm.reset();
     // open login modal
     openModal(loginModal);
-  } else { // if admin mode is on
+  } else {
+    // if admin mode is on
     // turn off admin mode
     adminMode = false;
     // hide addEventButton in navbar
-    addEventButton.classList.add('hidden');
+    addEventButton.classList.add("hidden");
     // hide edit and delete buttons on each event
-    const eventDivs = document.querySelectorAll('.eventDiv');
+    const eventDivs = document.querySelectorAll(".eventDiv");
     eventDivs.forEach((eventDiv) => {
-      const editButton = eventDiv.querySelector('.editButton');
-      const deleteButton = eventDiv.querySelector('.deleteButton');
-      editButton.classList.add('hidden');
-      deleteButton.classList.add('hidden');
+      const editButton = eventDiv.querySelector(".editButton");
+      const deleteButton = eventDiv.querySelector(".deleteButton");
+      editButton.classList.add("hidden");
+      deleteButton.classList.add("hidden");
     });
     // change Admin button text to "Admin"
-    adminButton.textContent = 'Admin';
+    adminButton.textContent = "Admin";
   }
 });
 
 // when user submits login form
-loginForm.addEventListener('submit', (event) => {
+loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   // check if entered username and password are correct
-  if (username.value === 'a' && password.value === 'a') {
+  if (username.value === "a" && password.value === "a") {
     // turn on admin mode
     adminMode = true;
     // close login modal
     closeModal();
     // show addEventButton in navbar
-    addEventButton.classList.remove('hidden');
+    addEventButton.classList.remove("hidden");
     // show edit and delete buttons on each event
-    const eventDivs = document.querySelectorAll('.eventDiv');
+    const eventDivs = document.querySelectorAll(".eventDiv");
     eventDivs.forEach((eventDiv) => {
-      const editButton = eventDiv.querySelector('.editButton');
-      const deleteButton = eventDiv.querySelector('.deleteButton');
-      editButton.classList.remove('hidden');
-      deleteButton.classList.remove('hidden');
+      const editButton = eventDiv.querySelector(".editButton");
+      const deleteButton = eventDiv.querySelector(".deleteButton");
+      editButton.classList.remove("hidden");
+      deleteButton.classList.remove("hidden");
     });
     // change Admin button text to "Logout"
-    adminButton.textContent = 'Logout';
+    adminButton.textContent = "Logout";
     // if number of events is 0, open add event modal
     if (numEvents === 0) {
       openModal(addEventModal);
     }
-  } else { // if entered username and password are incorrect
+  } else {
+    // if entered username and password are incorrect
     // show error message
-    console.log('Incorrect username or password');
+    alert("Incorrect username or password");
   }
 });
 
@@ -237,62 +237,65 @@ loginForm.addEventListener('submit', (event) => {
 //***The rest of this logic only occurs if admin mode is on***
 
 //2. when user clicks on addEventButton, user is presented with the add event modal
-//  a) if user enters all required fields, a new event is created stored in an event object
-//      -the event object is nested in a bigger object that stores all events
+//  a) if user enters all required fields,
 //      -the add event modal is closed
-//      -a new event referencing the event object is created and displayed in the events container,
 //      -the new event has edit and delete buttons
 //  b) if user does not enter all required fields, a new event is not created
 //      -user is presented with an error message
 //      -the add event modal is not closed
 
 // when user clicks on addEventButton
-addEventButton.addEventListener('click', () => {
+addEventButton.addEventListener("click", () => {
   // open add event modal
   openModal(addEventModal);
 });
 
 // when user submits add event form
-addEventForm.addEventListener('submit', (event) => {
+addEventForm.addEventListener("submit", (event) => {
   event.preventDefault();
   // check if all required fields are filled out
-  if (addEventTitle.value && addEventDate.value && addEventTime.value && addEventLocation.value && addEventDescription.value) {
-    // create new event object
-    const newEvent = {
-      title: addEventTitle.value,
-      date: addEventDate.value,
-      time: addEventTime.value,
-      location: addEventLocation.value,
-      description: addEventDescription.value
-    };
-    // add new event object to events object
-    events[`event${numEvents}`] = newEvent;
+  if (
+    addEventTitle.value &&
+    addEventDate.value &&
+    addEventTime.value &&
+    addEventLocation.value &&
+    addEventDescription.value
+  ) {
     // increase number of events
     numEvents++;
     // close add event modal
     closeModal();
     // create new event element and add it to the events container
-    const newEventElement = document.createElement('div');
-    newEventElement.classList.add('eventDiv');
-    newEventElement.innerHTML = `
-      <h3 class="eventTitle" id="eventTitle">${newEvent.title}</h3>
-      <p class="eventDescription" id="eventDescription">${newEvent.description}</p>
-      <p class="eventDate" id="eventDate">${newEvent.date}</p>
-      <p class="eventTime" id="eventTime">${newEvent.time}</p>
-      <p class="eventLocation" id="eventLocation">${newEvent.location}</p>
-      <button class="editButton">Edit</button>
-      <button class="deleteButton">Delete</button>
-    `;
-    eventsContainer.appendChild(newEventElement);
-  } else { // if required fields are not filled out
+    const newEvent = document.createElement("div");
+    newEvent.classList.add("eventDiv");
+    newEvent.innerHTML = `
+        <h2 class="eventTitle">${addEventTitle.value}</h2>
+        <p class="eventDescription">${addEventDescription.value}</p>
+        <p class="eventDate">${addEventDate.value}</p>
+        <p class="eventTime">${addEventTime.value}</p>
+        <p class="eventLocation">${addEventLocation.value}</p> 
+        <button class="editButton">Edit</button>
+        <button class="deleteButton">Delete</button>
+        `;
+    eventsContainer.appendChild(newEvent);
+    // reset add event form
+    addEventForm.reset();
+    // show edit and delete buttons on new event
+
+    const editButton = newEvent.querySelector(".editButton");
+    const deleteButton = newEvent.querySelector(".deleteButton");
+    editButton.classList.remove("hidden");
+    deleteButton.classList.remove("hidden");
+  } else {
+    // if all required fields are not filled out
     // show error message
-    console.log('Please fill out all required fields');
+    alert("Please fill out all required fields");
   }
 });
 
 /*************************************************************************** */
 
-//3. when user clicks on edit button, user is presented with the edit event modal
+//3. For all instances of editButton, when user clicks on edit button, user is presented with the edit event modal
 //  a) the modal feilds are populated with the current event information
 //  b) if user enters all required fields, the event is updated
 //      -the edit event modal is closed
@@ -302,55 +305,52 @@ addEventForm.addEventListener('submit', (event) => {
 //      -the edit event modal is not closed
 
 // when user clicks on edit button
-
-//3. when user clicks on edit button, user is presented with the edit event modal
-editButton.addEventListener("click", () => {
-  //  a) the modal feilds are populated with the current event information
-  editEventTitle.value = eventTitle.innerText;
-  editEventDate.value = eventDate.innerText;
-  editEventTime.value = eventTime.innerText;
-  editEventLocation.value = eventLocation.innerText;
-  editEventDescription.value = eventDescription.innerText;
-
-  //display the edit event modal
-  openModal(editEventModal);
-});
-
-//  b) if user enters all required fields, the event is updated
-editEventForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  //get the updated event information from the form
-  const updatedTitle = editEventTitle.value;
-  const updatedDate = editEventDate.value;
-  const updatedTime = editEventTime.value;
-  const updatedLocation = editEventLocation.value;
-  const updatedDescription = editEventDescription.value;
-
-  //check if all required fields have been entered
-  if (
-    updatedTitle !== "" &&
-    updatedDate !== "" &&
-    updatedTime !== "" &&
-    updatedLocation !== "" &&
-    updatedDescription !== ""
-  ) {
-    //  -the edit event modal is closed
-    closeModal();
-
-    //  -the updated event is displayed in the events container
-    eventTitle.innerText = updatedTitle;
-    eventDate.innerText = updatedDate;
-    eventTime.innerText = updatedTime;
-    eventLocation.innerText = updatedLocation;
-    eventDescription.innerText = updatedDescription;
-  } else {
-    //  c) if user does not enter all required fields, the event is not updated
-    //      -user is presented with an error message
-    alert("Please fill out all required fields");
-
-    //      -the edit event modal is not closed
+eventsContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("editButton")) {
+    // open edit event modal
+    openModal(editEventModal);
+    // populate modal fields with current event information
+    const eventDiv = event.target.parentElement;
+    const eventTitle = eventDiv.querySelector(".eventTitle").textContent;
+    const eventDescription = eventDiv.querySelector(".eventDescription")
+      .textContent;
+    const eventDate = eventDiv.querySelector(".eventDate").textContent;
+    const eventTime = eventDiv.querySelector(".eventTime").textContent;
+    const eventLocation = eventDiv.querySelector(".eventLocation").textContent;
+    editEventTitle.value = eventTitle;
+    editEventDescription.value = eventDescription;
+    editEventDate.value = eventDate;
+    editEventTime.value = eventTime;
+    editEventLocation.value = eventLocation;
+    // when user submits edit event form
+    editEventForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      // check if all required fields are filled out
+      if (
+        editEventTitle.value &&
+        editEventDate.value &&
+        editEventTime.value &&
+        editEventLocation.value &&
+        editEventDescription.value
+      ) {
+        // close edit event modal
+        closeModal();
+        // update event information
+        eventDiv.querySelector(".eventTitle").textContent =
+          editEventTitle.value;
+        eventDiv.querySelector(".eventDescription").textContent =
+          editEventDescription.value;
+        eventDiv.querySelector(".eventDate").textContent = editEventDate.value;
+        eventDiv.querySelector(".eventTime").textContent = editEventTime.value;
+        eventDiv.querySelector(".eventLocation").textContent =
+          editEventLocation.value;
+        // reset edit event form
+        editEventForm.reset();
+      } else {
+        // if all required fields are not filled out
+        // show error message
+        alert("Please fill out all required fields");
+      }
+    });
   }
 });
-
-
